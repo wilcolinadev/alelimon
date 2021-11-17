117d6938bf8ec9e768a188648c0c05452cb94366
Wilfredo R Colina <56809033+wilcodes@users.noreply.github.com>
2021-11-17 11:32:25 -0600

    via school pc
    nav completed

--- hamburger.js
+++ hamburger.js
@@ -13,15 +13,13 @@ function toggleMenu() {
         menu.classList.remove("showMenu");
         closeIcon.style.display = "none";
         menuIcon.style.display = "block";
-        modalLogoBlack.style.display = "none";
-        modalLogoWhite.style.display = "block";
+
 
     } else {
         menu.classList.add("showMenu");
         closeIcon.style.display = "block";
         menuIcon.style.display = "none";
-        modalLogoBlack.style.display = "block";
-        modalLogoWhite.style.display = "none";
+
 
     }
 }
